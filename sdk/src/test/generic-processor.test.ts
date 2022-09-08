8c7e84821e6eb7659f7d40cce017dfe8cc141090
philz3906 <philz3906@gmail.com>
2022-09-08 11:49:46 -0700

    chore(sdk): try include insource for user debugging (#84)

--- sdk/src/test/generic-processor.test.ts
+++ sdk/src/test/generic-processor.test.ts
@@ -24,12 +24,18 @@ describe('Test Generic Processor', () => {
       ctx.meter.Counter('event_num').add(1)
     })
 
+    GenericProcessor.bind('event WalletCreated(address wallet, address owner)', {
+      address: '0x57E037F4d2c8BEa011Ad8a9A5AF4AaEEd508650f',
+    }).onAllEvents(function (log, ctx) {
+      ctx.meter.Counter('wallet').add(1)
+    })
+
     await service.start()
   })
 
   test('check configuration', async () => {
     const config = await service.getConfig({})
-    expect(config.contractConfigs).length(1)
+    expect(config.contractConfigs).length(2)
     expect(config.contractConfigs?.[0].contract?.name).equals('Generic')
   })
 
@@ -39,6 +45,13 @@ describe('Test Generic Processor', () => {
     expect(counters).length(2)
     expect(firstCounterValue(res.result, 'event_num')).equals(1n)
     expect(counters?.[0].runtimeInfo?.from).equals(HandlerType.LOG)
+  })
+
+  test('Check log dispatch no buffer over rune', async () => {
+    const logStr =
+      '{"address":"0x57e037f4d2c8bea011ad8a9a5af4aaeed508650f","topics":["0x5b03bfed1c14a02bdeceb5fa582eb1a5765fc0bc64ca0e6af4c20afc9487f081"],"data":"0x00000000000000000000000093269483a70c68d5c5bb63aac1e8f4ac59f498800000000000000000000000000c520e51c055cf63bab075715c1b860b2e9b8e24","blockNumber":"0xc9d6d7","transactionHash":"0x208af3250499672c2f07138b9aa236153c65c78ae4341b23c2763017afdd61a2","transactionIndex":"0xf3","blockHash":"0x6e3b100c34b510049e922fbe1c1dab1b0793be3d1229b632688e6a518cdd11b6","logIndex":"0x14b","removed":false}'
+    const res = await service.testLog(JSON.parse(logStr))
+    console.log(JSON.stringify(res))
   })
 
   const logData = {
