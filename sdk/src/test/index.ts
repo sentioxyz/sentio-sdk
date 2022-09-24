af4ec6e316ae4c903eadd6ba229073107403b755
philz3906 <philz3906@gmail.com>
2022-09-24 15:46:11 -0700

    chore(sdk): export more test utils (#115)

--- sdk/src/test/index.ts
+++ sdk/src/test/index.ts
@@ -1,2 +1,3 @@
 export { TestProcessorServer } from './test-processor-server'
 export { loadTestProvidersFromEnv } from './test-provider'
+export { MetricValueToNumber, firstCounterValue, firstGaugeValue } from './metric-utils'
