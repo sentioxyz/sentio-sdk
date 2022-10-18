fd58fbd7593da2299f43a20c581ac7039e95a4a1
philz3906 <philz3906@gmail.com>
2022-10-18 14:31:41 -0700

    chore(sdk): adjust namespace for easier doc gen (#178)

--- sdk/src/builtin/index.ts
+++ sdk/src/builtin/index.ts
@@ -3,3 +3,5 @@ export * as erc20 from './erc20'
 export * as erc20bytes from './erc20bytes'
 export * as weth9 from './weth9'
 export * as solana from './solana'
+
+export * as aptos from 'builtin/aptos'
