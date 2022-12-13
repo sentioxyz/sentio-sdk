95e30e3eab556c3c14bb19b400debe8ba2e9a019
philz3906 <philz3906@gmail.com>
2022-12-12 22:45:26 -0800

    feat(sdk): release with better streaming process API (#245)

--- sdk/templates/evm/src/processor.ts
+++ sdk/templates/evm/src/processor.ts
@@ -7,7 +7,7 @@ const rewardPerBlock = Gauge.register('reward_per_block', {
   description: 'rewards for each block grouped by phase',
   unit: 'x2y2',
 })
-const tokenCounter = new Counter('token')
+const tokenCounter = Counter.register('token')
 
 X2y2Processor.bind({ address: '0xB329e39Ebefd16f40d38f07643652cE17Ca5Bac1' }).onBlockInterval(async (_, ctx) => {
   const phase = (await ctx.contract.currentPhase()).toString()
