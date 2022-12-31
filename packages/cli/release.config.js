96d29b0551959f3573b6169d9408d316650e7f60
philz3906 <philz3906@gmail.com>
2022-12-31 07:22:55 -0800

    chore(release): merge sdk and cli release action

--- packages/cli/release.config.js
+++ packages/cli/release.config.js
@@ -16,6 +16,7 @@ module.exports = {
       },
     ],
     ["@semantic-release/npm", {
+      npmPublish: true,
     }],
     ['@semantic-release/release-notes-generator', {
       preset: 'conventionalcommits',
