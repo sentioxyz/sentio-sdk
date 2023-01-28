8ea9c360a145fcf06d0330b8b3757bb51b67e2db
philz3906 <philz3906@gmail.com>
2023-01-28 11:12:54 -0800

    chore(sdk): improve cache performance of getPrice (#304)

--- packages/sdk/src/utils/price.ts
+++ packages/sdk/src/utils/price.ts
@@ -1,4 +1,4 @@
-import { PriceServiceClient, PriceServiceDefinition } from '@sentio/protos/lib/service/price/protos/price'
+import { CoinID, PriceServiceClient, PriceServiceDefinition } from '@sentio/protos/lib/service/price/protos/price'
 import { createChannel, createClientFactory } from 'nice-grpc'
 import { retryMiddleware, RetryOptions } from 'nice-grpc-client-middleware-retry'
 import { Endpoints } from '@sentio/runtime'
@@ -12,8 +12,52 @@ export function getPriceClient(address?: string) {
   return createClientFactory().use(retryMiddleware).create(PriceServiceDefinition, channel)
 }
 
-const priceMap = new Map<string, number>()
+const priceMap = new Map<string, Promise<number>>()
 let priceClient: PriceServiceClient<RetryOptions>
+
+export async function getPriceByTypeOrSymbol(date: Date, coinId: CoinID): Promise<number> {
+  if (!priceClient) {
+    priceClient = getPriceClient()
+  }
+
+  const dateStr = dateString(date)
+  const todayDateString = dateString(new Date())
+
+  let key: string
+  if (coinId.symbol) {
+    key = `${coinId.symbol}-${dateStr}`
+  } else {
+    key = `${coinId.address?.address}-${coinId.address?.chain}-${dateStr}`
+  }
+  let price = priceMap.get(key)
+  if (price) {
+    return price
+  }
+
+  const response = priceClient.getPrice(
+    {
+      timestamp: date,
+      coinId,
+    },
+    {
+      retry: true,
+      retryMaxAttempts: 8,
+    }
+  )
+  price = response.then((res) => {
+    if (res.timestamp) {
+      const responseDateString = dateString(res.timestamp)
+      if (responseDateString !== dateStr || responseDateString !== todayDateString) {
+        priceMap.delete(key)
+      }
+    } else {
+      priceMap.delete(key)
+    }
+    return res.price
+  })
+  priceMap.set(key, price)
+  return price
+}
 
 /**
  *
@@ -21,38 +65,13 @@ let priceClient: PriceServiceClient<RetryOptions>
  * @param coinType
  * @param date
  */
-export async function getPriceByType(chainId: string, coinType: string, date: Date): Promise<number> {
-  if (!priceClient) {
-    priceClient = getPriceClient()
-  }
-
-  const dateStr = dateString(date)
-  const key = `${coinType}-${dateStr}`
-  let price = priceMap.get(key)
-  if (price) {
-    return price
-  }
-
-  const response = await priceClient.getPrice(
-    {
-      timestamp: date,
-      coinId: {
-        address: {
-          chain: chainId,
-          address: coinType,
-        },
-      },
+async function getPriceByType(chainId: string, coinType: string, date: Date): Promise<number> {
+  return getPriceByTypeOrSymbol(date, {
+    address: {
+      chain: chainId,
+      address: coinType,
     },
-    {
-      retry: true,
-      retryMaxAttempts: 8,
-    }
-  )
-  price = response.price
-  if (response.timestamp && dateString(response.timestamp) === dateStr) {
-    priceMap.set(key, price)
-  }
-  return price
+  })
 }
 
 /**
@@ -61,34 +80,7 @@ export async function getPriceByType(chainId: string, coinType: string, date: Da
  * @param date
  */
 export async function getPriceBySymbol(symbol: string, date: Date): Promise<number> {
-  if (!priceClient) {
-    priceClient = getPriceClient()
-  }
-
-  const dateStr = dateString(date)
-  const key = `${symbol}-${dateStr}`
-  let price = priceMap.get(key)
-  if (price) {
-    return price
-  }
-
-  const response = await priceClient.getPrice(
-    {
-      timestamp: date,
-      coinId: {
-        symbol,
-      },
-    },
-    {
-      retry: true,
-      retryMaxAttempts: 8,
-    }
-  )
-  price = response.price
-  if (response.timestamp && dateString(response.timestamp) === dateStr) {
-    priceMap.set(key, price)
-  }
-  return price
+  return getPriceByTypeOrSymbol(date, { symbol })
 }
 
 function dateString(date: Date) {
