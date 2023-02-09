5488cbcfc4313d6817d4f572d293d5de1011ca2d
philz3906 <philz3906@gmail.com>
2023-02-09 10:25:00 -0800

    chore(price): tolerate price result within one day by default, can be… (#364)

--- packages/sdk/src/utils/price.ts
+++ packages/sdk/src/utils/price.ts
@@ -15,7 +15,11 @@ export function getPriceClient(address?: string) {
 const priceMap = new Map<string, Promise<number>>()
 let priceClient: PriceServiceClient<RetryOptions>
 
-async function getPriceByTypeOrSymbol(date: Date, coinId: CoinID): Promise<number> {
+interface PriceOptions {
+  toleranceInDays?: number
+}
+
+async function getPriceByTypeOrSymbol(date: Date, coinId: CoinID, options?: PriceOptions): Promise<number> {
   if (!priceClient) {
     priceClient = getPriceClient()
   }
@@ -47,8 +51,16 @@ async function getPriceByTypeOrSymbol(date: Date, coinId: CoinID): Promise<numbe
   price = response.then((res) => {
     if (res.timestamp) {
       const responseDateString = dateString(res.timestamp)
-      if (responseDateString !== dateStr || responseDateString === todayDateString) {
+      if (responseDateString === todayDateString) {
         priceMap.delete(key)
+      } else {
+        // https://www.javatpoint.com/javascript-date-difference
+        const diff = Math.abs(res.timestamp.getTime() - date.getTime())
+        const daysDiff = diff / (1000 * 60 * 60 * 24)
+        const tolerance = options?.toleranceInDays || 1
+        if (daysDiff > tolerance) {
+          priceMap.delete(key)
+        }
       }
     } else {
       priceMap.delete(key)
@@ -65,13 +77,22 @@ async function getPriceByTypeOrSymbol(date: Date, coinId: CoinID): Promise<numbe
  * @param coinType
  * @param date
  */
-export async function getPriceByType(chainId: string, coinType: string, date: Date): Promise<number> {
-  return getPriceByTypeOrSymbol(date, {
-    address: {
-      chain: chainId,
-      address: coinType,
+export async function getPriceByType(
+  chainId: string,
+  coinType: string,
+  date: Date,
+  options?: PriceOptions
+): Promise<number> {
+  return getPriceByTypeOrSymbol(
+    date,
+    {
+      address: {
+        chain: chainId,
+        address: coinType,
+      },
     },
-  })
+    options
+  )
 }
 
 /**
@@ -79,8 +100,8 @@ export async function getPriceByType(chainId: string, coinType: string, date: Da
  * @param symbol token symbol like BTC, etc
  * @param date
  */
-export async function getPriceBySymbol(symbol: string, date: Date): Promise<number> {
-  return getPriceByTypeOrSymbol(date, { symbol })
+export async function getPriceBySymbol(symbol: string, date: Date, options?: PriceOptions): Promise<number> {
+  return getPriceByTypeOrSymbol(date, { symbol }, options)
 }
 
 function dateString(date: Date) {
