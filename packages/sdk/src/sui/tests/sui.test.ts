67be38cb622fad2b5a4ea8124e158156ab7aa4bc
philz3906 <philz3906@gmail.com>
2023-04-11 03:32:24 -0700

    chore(sdk): upgrade sui sdk (#463)

--- packages/sdk/src/sui/tests/sui.test.ts
+++ packages/sdk/src/sui/tests/sui.test.ts
@@ -65,7 +65,7 @@ const testData = {
             type: 'object',
             objectType: 'sharedObject',
             objectId: '0x0000000000000000000000000000000000000000000000000000000000000005',
-            initialSharedVersion: 1,
+            initialSharedVersion: '1',
             mutable: true,
           },
           {
@@ -195,8 +195,8 @@ const testData = {
           },
         ],
         owner: '0x4928b3478f47a5f845096fbe08cbaaa1e9122f8a3a3634c8454e2bafc46b7ad8',
-        price: 1000,
-        budget: 10227000,
+        price: '1000',
+        budget: '10227000',
       },
     },
     txSignatures: [
@@ -220,79 +220,79 @@ const testData = {
     modifiedAtVersions: [
       {
         objectId: '0x0000000000000000000000000000000000000000000000000000000000000005',
-        sequenceNumber: 280243,
+        sequenceNumber: '280243',
       },
       {
         objectId: '0x06549c4fa56f5c55b45f2bb6d5e41c9ffe93322c9c0cf8a6fa332e03d57cd818',
-        sequenceNumber: 248215,
+        sequenceNumber: '248215',
       },
       {
         objectId: '0x6af2a2b7ca60bf76174adfd3e9c4957f8e937759603182f9b46c7f6c5f19c6d2',
-        sequenceNumber: 280243,
+        sequenceNumber: '280243',
       },
       {
         objectId: '0x09aa7eb7348209863a467ec19ba43f06b009d79d1b3bbcedc45804bf99d83eac',
-        sequenceNumber: 248215,
+        sequenceNumber: '248215',
       },
       {
         objectId: '0x784d237927050a245a9ba5e0354ec8fea5b69bfaf37aaada70f8acc5b72e7e44',
-        sequenceNumber: 10210,
+        sequenceNumber: '10210',
       },
       {
         objectId: '0x7bca72a25735ab5becda8537878cc13cb811879adaef531d23bcc6edc92d2d6c',
-        sequenceNumber: 10210,
+        sequenceNumber: '10210',
       },
       {
         objectId: '0x9319084a620be0e87b3b17f4ae26dfb51fdbfa2d7bb439e7dfc046a1de091ec8',
-        sequenceNumber: 10210,
+        sequenceNumber: '10210',
       },
       {
         objectId: '0xa20665dfd4d6a527c0d4a502f1523014d10494deb4c4ff46becf606cbbb437a1',
-        sequenceNumber: 10210,
+        sequenceNumber: '10210',
       },
       {
         objectId: '0xacb23c3e52f27eddd94bc78b45028ee40ed1cc3da6445a7c24c20e68593e6d5a',
-        sequenceNumber: 10400,
+        sequenceNumber: '10400',
       },
       {
         objectId: '0xb0ed1bac02fa29a039b9951fedab90938fc2b2f4798bcebf731f909d36522736',
-        sequenceNumber: 10400,
+        sequenceNumber: '10400',
       },
       {
         objectId: '0xbc9e291e42202c0e74eeefa9a6f391735fc92d9e350be7d8553f1c1e0c7b0d61',
-        sequenceNumber: 10210,
+        sequenceNumber: '10210',
       },
       {
         objectId: '0xc1249d21c5cd64069fcee8f27cebb7a0dfce5494fb266c5e941ea5791118f4cc',
-        sequenceNumber: 10210,
+        sequenceNumber: '10210',
       },
       {
         objectId: '0xc7658384ee1298bb43107794ccae10d7a13dc6544043299affea621116170bcd',
-        sequenceNumber: 10210,
+        sequenceNumber: '10210',
       },
       {
         objectId: '0xdde6e8a18c4717054b9abef47f0b3b4fa8c3c9d670bf59fd3b11ac6478d17668',
-        sequenceNumber: 10400,
+        sequenceNumber: '10400',
       },
       {
         objectId: '0xe1f247f4edf3326161f74115470afe5c51c8700843ae594dfa4e82780a6fdc3b',
-        sequenceNumber: 10400,
+        sequenceNumber: '10400',
       },
       {
         objectId: '0xe5181942681d8b49afbb4d70804148e2a6a837b6887ba8967d6254b71d3195e0',
-        sequenceNumber: 10210,
+        sequenceNumber: '10210',
       },
       {
         objectId: '0xee94d0332dfe9cbf7cbc25bdc2a21e2ed3fc48d18edab2b2121cb6a2f3a7c358',
-        sequenceNumber: 10210,
+        sequenceNumber: '10210',
       },
       {
         objectId: '0xf14c33c7419ccf21b75d82c78350e41b4096e411d8f2cbf00fa01f0ab9b21c63',
-        sequenceNumber: 10400,
+        sequenceNumber: '10400',
       },
       {
         objectId: '0xf673811fe05efd3b9c40f8dac6f44a6af080377d0149c6e5304167fab84ba579',
-        sequenceNumber: 10210,
+        sequenceNumber: '10210',
       },
     ],
     sharedObjects: [
@@ -455,7 +455,7 @@ const testData = {
     {
       id: {
         txDigest: '1Jo5RUSfLknKiGkZUxVxkWhTbX7CD3Jm3fQzaCEV3JM',
-        eventSeq: 0,
+        eventSeq: '0',
       },
       packageId: '0x0000000000000000000000000000000000000000000000000000000000000003',
       transactionModule: 'sui_system',
@@ -482,8 +482,8 @@ const testData = {
       },
       objectType: '0x3::sui_system::SuiSystemState',
       objectId: '0x0000000000000000000000000000000000000000000000000000000000000005',
-      version: 280244,
-      previousVersion: 280243,
+      version: '280244',
+      previousVersion: '280243',
       digest: '395mZycSW6f4hRQbToe4UuEvc5QHcXJxAiNYPjnf9UD5',
     },
     {
@@ -494,8 +494,8 @@ const testData = {
       },
       objectType: '0x2::coin::Coin<0x2::sui::SUI>',
       objectId: '0x06549c4fa56f5c55b45f2bb6d5e41c9ffe93322c9c0cf8a6fa332e03d57cd818',
-      version: 280244,
-      previousVersion: 248215,
+      version: '280244',
+      previousVersion: '248215',
       digest: 'EFrBdvNNQyjoXkBRGQnSmj1ELV18a1p7hb4dnuxKWPyY',
     },
     {
@@ -506,8 +506,8 @@ const testData = {
       },
       objectType: '0x2::dynamic_field::Field<u64, 0x3::sui_system_state_inner::SuiSystemStateInner>',
       objectId: '0x6af2a2b7ca60bf76174adfd3e9c4957f8e937759603182f9b46c7f6c5f19c6d2',
-      version: 280244,
-      previousVersion: 280243,
+      version: '280244',
+      previousVersion: '280243',
       digest: 'D7UBCDNU5TPXxcQzyVLBU6mdfoafQBFqKsYi4oYXrnvr',
     },
     {
@@ -518,7 +518,7 @@ const testData = {
       },
       objectType: '0x3::staking_pool::StakedSui',
       objectId: '0x746cb3749d743d9f2f9384df3c6d9076ffa0ed748b4e827508632e167be3973f',
-      version: 280244,
+      version: '280244',
       digest: 'AWojdmyZ6dSG56J2zYsyxKGJupR6yFqgNhPsbT2kirZ',
     },
   ],
@@ -531,6 +531,6 @@ const testData = {
       amount: '-1014399901',
     },
   ],
-  timestampMs: 1680304142717,
-  checkpoint: 233041,
+  timestampMs: '1680304142717',
+  checkpoint: '233041',
 }
