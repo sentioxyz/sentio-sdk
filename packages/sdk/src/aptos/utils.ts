564bbac5617d0ebf92ee9d3bd1eeae4d56ac331b
philz3906 <philz3906@gmail.com>
2023-05-02 12:08:59 +0800

    fix(sdk/aptos): no normalization for aptos for now (#509)

--- packages/sdk/src/aptos/utils.ts
+++ packages/sdk/src/aptos/utils.ts
@@ -1,5 +1,5 @@
 import { HexString, TxnBuilderTypes } from 'aptos-sdk'
-import { isFrameworkAccount } from '../move/index.js'
+// import { isFrameworkAccount } from '../move/index.js'
 
 export function isValidAptosAddress(value: string): boolean {
   return TxnBuilderTypes.AccountAddress.isValid(value)
@@ -7,8 +7,9 @@ export function isValidAptosAddress(value: string): boolean {
 
 export function validateAndNormalizeAddress(address: string): string {
   const hex = TxnBuilderTypes.AccountAddress.fromHex(address).toHexString()
-  if (isFrameworkAccount(address)) {
-    return HexString.ensure(hex).toShortString()
-  }
-  return hex.toString()
+  // if (isFrameworkAccount(address)) {
+  //   return HexString.ensure(hex).toShortString()
+  // }
+  // return hex.toString()
+  return HexString.ensure(hex).toShortString()
 }
