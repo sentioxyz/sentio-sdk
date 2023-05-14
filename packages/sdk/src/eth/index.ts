806b58ed14d0cfdd9b0c0d6cfd17ed2b6ee77b61
philz3906 <philz3906@gmail.com>
2023-05-14 11:45:50 +0800

    fix(sdk): fix account context export (#528)

--- packages/sdk/src/eth/index.ts
+++ packages/sdk/src/eth/index.ts
@@ -7,7 +7,7 @@ export { EthContext } from './context.js'
 export * from './eth.js'
 export { BindOptions, AccountBindOptions } from './bind-options.js'
 export { getProcessor, addProcessor, getContractByABI, addContractByABI } from './binds.js'
-export { ContractContext, GlobalContext, ContractView, BoundContractView } from './context.js'
+export { AccountContext, ContractContext, GlobalContext, ContractView, BoundContractView } from './context.js'
 
 export { EthPlugin } from './eth-plugin.js'
 
