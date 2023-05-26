4d5aaa99a6ba01fa8f4045c88c98d25889aa28d6
philz3906 <philz3906@gmail.com>
2023-05-26 21:26:43 +0800

    chore(sdk/util): provide an util funtion to treat eth exception as und… (#555)

---
+++ packages/sdk/src/utils/call.ts
@@ -0,0 +1,15 @@
+/**
+ * ignore eth call exception and return undefined if that happened
+ * @param call
+ */
+async function ignoreCallException<Res>(call: () => Promise<Res>): Promise<Res | undefined> {
+  try {
+    return await call()
+  } catch (err) {
+    if (err.code === 'CALL_EXCEPTION') {
+      console.log('eth call exception, return undefined', err)
+      return undefined
+    }
+    throw err
+  }
+}
