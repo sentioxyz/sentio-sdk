01f4009be602c425b27867d37a91d72451449c37
philz3906 <philz3906@gmail.com>
2023-07-09 22:12:47 +0800

    fix(sdk): fix aptos processor export (#588)

--- packages/sdk/src/aptos/index.ts
+++ packages/sdk/src/aptos/index.ts
@@ -2,6 +2,7 @@ export * from './move-types.js'
 export type { TypedEventInstance, TypedFunctionPayload, TypedMoveResource } from './models.js'
 export { defaultMoveCoder, MoveCoder } from './move-coder.js'
 export { AptosBaseProcessor, AptosResourcesProcessor, AptosModulesProcessor } from './aptos-processor.js'
+export { AptosResourceProcessorTemplate } from './aptos-resource-processor-template.js'
 export { AptosContext, AptosResourcesContext } from './context.js'
 export { AptosBindOptions, AptosNetwork } from './network.js'
 export * from './api.js'
@@ -11,4 +12,4 @@ export { AptosPlugin } from './aptos-plugin.js'
 
 export { BUILTIN_TYPES } from '../move/types.js'
 
-export * from './utils.js'
+// export * from './utils.js'
