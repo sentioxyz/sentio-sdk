e8d8d2846aaba9de3f6dd7595f1bc485124de3d6
Ping Chen <remotenonsense@gmail.com>
2024-05-12 00:41:53 +0900

    chore(deps): use pnpm v9 and tsx (#723)

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -3,6 +3,8 @@
 set -e
 
 pnpm install
+pnpm --filter "./packages/protos" build
+pnpm --filter "./packages/runtime" build
 pnpm --filter "./packages/*" -filter "./examples/*" build
 
 pnpm --filter "./packages/cli/templates/**"  build --skip-deps
