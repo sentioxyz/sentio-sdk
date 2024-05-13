d29fdbd60f69d0d8ce081ebf3ce00baaa0ee40b5
Ping Chen <remotenonsense@gmail.com>
2024-05-13 09:46:31 +0900

    fix(deps): remove peerDependencies from runtime (#730)

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -3,8 +3,6 @@
 set -e
 
 pnpm install
-pnpm --filter "./packages/protos" build
-pnpm --filter "./packages/runtime" build
 pnpm --filter "./packages/*" -filter "./examples/*" build
 
 pnpm --filter "./packages/cli/templates/**"  build --skip-deps
