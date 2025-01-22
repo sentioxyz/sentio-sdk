aacf4b6ba3bbf5b885a1523995fedb6940b20088
Mo ShakeG <40227090+mshakeg@users.noreply.github.com>
2025-01-22 10:13:28 +0400

    feat: expose memory database for testing (#1134)

--- packages/sdk/src/testing/index.ts
+++ packages/sdk/src/testing/index.ts
@@ -2,3 +2,5 @@ export { TestProcessorServer } from './test-processor-server.js'
 export { MetricValueToNumber, firstCounterValue, firstGaugeValue } from './metric-utils.js'
 
 export { loadTestProvidersFromEnv } from './test-provider.js' // TODO make the interface more standard and then export
+
+export { MemoryDatabase, withStoreContext } from './memory-database.js'
