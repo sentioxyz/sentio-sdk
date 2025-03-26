44a5042a4ff2f51100067d0afb2cff5e3b4b0c0c
Ping Chen <remotenonsense@gmail.com>
2025-03-26 20:15:34 +0900

    chore(sdk): remove unused event-tracker

--- packages/sdk/src/core/core-plugin.ts
+++ packages/sdk/src/core/core-plugin.ts
@@ -3,7 +3,6 @@ import { ProcessConfigResponse } from '@sentio/protos'
 
 import { MetricState } from './meter.js'
 import { ExporterState } from './exporter.js'
-import { EventTrackerState } from './event-tracker.js'
 import { TemplateInstanceState } from './template.js'
 import { EventLoggerState } from './event-logger.js'
 import { DatabaseSchemaState } from './database-schema.js'
@@ -24,18 +23,6 @@ export class CorePlugin extends Plugin {
     for (const event of EventLoggerState.INSTANCE.getValues()) {
       config.eventLogConfigs.push({
         ...event.config
-      })
-    }
-
-    // eslint-disable-next-line deprecation/deprecation
-    for (const eventTracker of EventTrackerState.INSTANCE.getValues()) {
-      config.eventTrackingConfigs.push({
-        distinctAggregationByDays: eventTracker.options.distinctByDays || [],
-        eventName: eventTracker.name,
-        retentionConfig: undefined,
-        totalByDay: eventTracker.options.totalByDay || false,
-        totalPerEntity: undefined,
-        unique: eventTracker.options.unique || false
       })
     }
 
--- packages/sdk/src/core/index.ts
+++ packages/sdk/src/core/index.ts
@@ -5,7 +5,6 @@ export { BaseContext } from './base-context.js'
 export { normalizeLabels } from './normalization.js'
 export { CounterBinding, Meter, type Labels, GaugeBinding, Counter, Gauge, MetricOptions } from './meter.js'
 export { Exporter } from './exporter.js'
-export * from './event-tracker.js' // TODO remove this
 export * from './event-logger.js'
 export { type Numberish, toBigInteger, toMetricValue } from './numberish.js'
 
