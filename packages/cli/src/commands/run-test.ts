d80c107c87b4be24c33176a059be54c0bd1d4c13
Ping Chen <remotenonsense@gmail.com>
2025-04-03 16:06:35 +0900

    chore(cli): add help text for sentio test command (#1227)

--- packages/cli/src/commands/run-test.ts
+++ packages/cli/src/commands/run-test.ts
@@ -1,12 +1,47 @@
 import { execSync } from 'child_process'
 import path from 'path'
+import commandLineArgs from 'command-line-args'
+import commandLineUsage from 'command-line-usage'
 import { getPackageRoot } from '../utils.js'
 
+const buildOptionDefinitions = [
+  {
+    name: 'help',
+    alias: 'h',
+    type: Boolean,
+    description: 'Display this usage guide.'
+  },
+  { name: 'test-only', description: "run tests with 'only' option set" },
+  { name: 'test-name-pattern', description: 'run tests whose name matches this regular expression' },
+  { name: 'test-skip-pattern', description: 'run tests whose name do not match this regular expression' }
+]
+
 export function runTest(argv: string[]) {
+  const options = commandLineArgs(buildOptionDefinitions, { argv, partial: true })
+  const usage = commandLineUsage([
+    {
+      header: 'Sentio test runner',
+      content: 'sentio test [options]'
+    },
+    {
+      header: 'Documentation',
+      content: 'All the options are piped to node test runner, see https://nodejs.org/api/test.html'
+    },
+    {
+      header: 'Options',
+      optionList: buildOptionDefinitions
+    }
+  ])
+
+  if (options.help) {
+    console.log(usage)
+    process.exit(0)
+  }
+
   const tsx = path.resolve(getPackageRoot('tsx'), 'dist', 'cli.mjs')
 
   // Run tsc to do check before test
   const tsc = path.resolve(getPackageRoot('typescript'), 'bin', 'tsc')
   execSync(`node ${tsc} --noEmit`, { stdio: 'inherit' })
-  execSync(`node ${tsx} --test **/*.test.ts ${argv.join(' ')}`, { stdio: 'inherit' })
+  execSync(`node ${tsx} --test ${argv.join(' ')}`, { stdio: 'inherit' })
 }
