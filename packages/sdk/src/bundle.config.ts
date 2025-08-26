f7fe433e025279e43b7e1b341667aba041b7f84b
Yulong <allendragon@gmail.com>
2025-08-26 15:31:47 +0800

    fix(bundle): add graphql to external dependencies in bundle and tsup … (#1346)

--- packages/sdk/src/bundle.config.ts
+++ packages/sdk/src/bundle.config.ts
@@ -21,5 +21,5 @@ export default defineConfig({
   // dts: true,
   format: 'esm',
   keepNames: true,
-  external: [...external, /^@sentio\/(chain|runtime).*$/, /^piscina.*$/]
+  external: [...external, /^@sentio\/(chain|runtime).*$/, /^piscina.*$/, /^graphql.*$/]
 })
