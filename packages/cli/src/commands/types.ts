b7ccaab2a6f46313729556819dab14f3b1502fa6
Fuyao Zhao <zfy0701@gmail.com>
2025-09-08 05:46:32 +0800

    .

--- packages/cli/src/commands/types.ts
+++ packages/cli/src/commands/types.ts
@@ -1,3 +1,3 @@
 import { Command } from '@commander-js/extra-typings'
 
-export type CommandOptionsType<T extends (...args: any[]) => Command> = ReturnType<ReturnType<T>['opts']>
+export type CommandOptionsType<T extends (...args: any[]) => Command<any, any, any>> = ReturnType<ReturnType<T>['opts']>
