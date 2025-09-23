da6296be3c1a64125a761dc68a6c9b0d235cd1c6
Yulong <allendragon@gmail.com>
2025-09-23 15:56:36 +0800

    chore(core): remove unused MeterNew and EventLoggerBindingNew imports (#1373)

--- packages/sdk/src/core/index.ts
+++ packages/sdk/src/core/index.ts
@@ -3,18 +3,7 @@ export type { PromiseOrValue, PromiseOrVoid } from './promises.js'
 export type { PartiallyOptional } from './partial-optional.js'
 export { BaseContext } from './base-context.js'
 export { normalizeLabels } from './normalization.js'
-export {
-  CounterBinding,
-  Meter,
-  type Labels,
-  GaugeBinding,
-  Counter,
-  Gauge,
-  MetricOptions,
-  CounterNew,
-  GaugeNew,
-  MeterNew
-} from './meter.js'
+export { CounterBinding, Meter, type Labels, GaugeBinding, Counter, Gauge, MetricOptions } from './meter.js'
 export { Exporter } from './exporter.js'
 export * from './event-logger.js'
 export { type Numberish, toBigInteger, toMetricValue } from './numberish.js'
