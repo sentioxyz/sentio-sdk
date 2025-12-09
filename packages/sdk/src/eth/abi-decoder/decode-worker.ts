558b7ac9cfc7935e6ca75db2b375162a7fcd7685
Yulong <allendragon@gmail.com>
2025-12-09 14:42:10 +0800

    chore(sdk): refactor transaction receipt and log formatting (#1411)

--- packages/sdk/src/eth/abi-decoder/decode-worker.ts
+++ packages/sdk/src/eth/abi-decoder/decode-worker.ts
@@ -1,6 +1,7 @@
 import { Interface, LogDescription, LogParams, ParamType, Result } from 'ethers'
 import { Piscina } from 'piscina'
 import { ServerError, Status } from 'nice-grpc'
+import { FormattedLog } from '../eth.js'
 
 export interface IResult {
   array?: any[]
@@ -42,13 +43,17 @@ export function parseLogInline(contractViewInterface: any, log: LogParams): LogD
   if (!log) {
     throw new ServerError(Status.INVALID_ARGUMENT, 'Log is empty')
   }
-  const logParam = log as any as { topics: Array<string>; data: string }
-  return contractViewInterface.parseLog(logParam)
+  const topics = log.topics
+  const data = log.data
+  return contractViewInterface.parseLog({ topics, data })
 }
 
-export function parseLog({ log }: { log: LogParams }): ILogDescription | null {
+export function parseLog({ log }: { log: FormattedLog }): ILogDescription | null {
   const fragments = Piscina['workerData']
   const contractViewInterface = new Interface(fragments)
+  if (log.raw) {
+    log = new FormattedLog(log.raw)
+  }
   const result = parseLogInline(contractViewInterface, log)
   if (!result) {
     return null
